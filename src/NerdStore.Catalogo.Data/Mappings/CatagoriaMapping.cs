d575d002a073631bc80e5fa5166af3af864a81ac
vainercesario <c.vainer@gmail.com>
2021-07-23 13:34:10 -0300

    Criação do repository

---
+++ src/NerdStore.Catalogo.Data/Mappings/CatagoriaMapping.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class CatagoriaMapping : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.HasKey(keyExpression: c=> c.Id);
+
+            builder.Property(c => c.Nome)
+                    .IsRequired()
+                    .HasColumnType("varchar(250)");
+
+            builder.HasMany(navigationExpression: c => c.Produtos)
+                .WithOne(navigationExpression: p => p.Categoria)
+                .HasForeignKey(p => p.CategoriaId);
+
+            builder.ToTable("Categorias");
+        }
+    }
+}
